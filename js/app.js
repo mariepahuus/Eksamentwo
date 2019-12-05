6e6ac7b4f0d96f8b23fba550060be467855cf731
mariepahuus <mari54u0@easv365.dk>
2019-12-05 12:13:31 +0100

    forsideigen

--- js/app.js
+++ js/app.js
@@ -2,3 +2,21 @@ new Vue({
   el: '#app',
   vuetify: new Vuetify(),
 })
+
+function myFunction() {
+  var x = document.getElementById("popup_1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("popup_2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
